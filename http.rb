882f0fc763d5c22a7e14e172cd8687de6e1dc9e9
Tomoya Ueda <to.ueda82@gmail.com>
2011-07-30 19:19:01 +0900

    細かく習性

--- http.rb
+++ http.rb
@@ -2,14 +2,20 @@
 require 'sinatra'
 
 get '/helloAjax' do
-  'Hello Ajax!!!'
+  puts params[:text] 
+  'Hello Ajax!!'
 end
 
 get '/echo' do
-  params[:text] + "を送信しましたね"
+  text = params[:text]
+  if params[:text]
+    params[:text] + "を送信しましたね"
+  else
+    ""
+  end 
 end
 
-#非同期のテスト用メソッド
+#非同期テスト用メソッド
 get '/sleep' do
   i = params[:text]
   sleep i.to_i
